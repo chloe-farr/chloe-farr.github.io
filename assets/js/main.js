52650130cc9d8b6b8f5bbe26801cf16611dc4020
chloe-farr <chloe.l.farr@gmail.com>
2026-03-05 00:11:43 -0800

    test resolve buttons failing

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,17 @@
 		$main = $('#main'),
 		$main_articles = $main.children('article');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	// Breakpoints (only if the library is loaded).
+		if (typeof breakpoints !== 'undefined') {
+			breakpoints({
+				xlarge:   [ '1281px',  '1680px' ],
+				large:    [ '981px',   '1280px' ],
+				medium:   [ '737px',   '980px'  ],
+				small:    [ '481px',   '736px'  ],
+				xsmall:   [ '361px',   '480px'  ],
+				xxsmall:  [ null,      '360px'  ]
+			});
+		}
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
